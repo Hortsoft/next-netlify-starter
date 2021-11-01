f4068fd63045b8a5529eb12475f112049d07838e
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-11-02 00:19:37 +1300

    Framer animation

--- pages/index.js
+++ pages/index.js
@@ -23,29 +23,13 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-        <motion.div initial="hidden" animate="visible" variants={{
-  hidden: {
-    scale: .8,
-    opacity: 0
-  },
-  visible: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      delay: .4
-    }
-  },
-}}>
-  <h1 className="title">
-    Wubba Lubba Dub Dub!
-  </h1>
-</motion.div>
-        </p>
+       
+        
         <motion.div
     style={styles}
     animate={{ scale: 3 }}
     transition={{ duration: 0.25 }}
-  />
+  /></p>
       </main>
 
       <Footer />
