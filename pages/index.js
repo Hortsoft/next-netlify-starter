fb6e68ca506bbe82abd03cc96173520228bd074f
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-11-14 00:40:59 +1300

    ;;l

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import FunctionClick from '@components/FunctionClick' 
- 
+import ModalImage from "react-modal-image";
 
 export default function Home() {
 
