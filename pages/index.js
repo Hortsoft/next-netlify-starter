b02a4db99c84ffb44526c27531ebfd3c6cd8a7aa
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-12-01 23:33:16 +1300

    class event

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FunctionClick from "@components/FunctionClick";
+import ClassClick from "@components/ClassClick";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description"> Hello World!!!</p>
         <FunctionClick />
+        <ClassClick />
       </main>
       <pre>"I don't trust those tomatoes. They seem kind of round."</pre>
       <Footer />
